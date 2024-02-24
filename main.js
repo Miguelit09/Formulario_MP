812d54b5285dd4e848f4f349a949925dffe37e3d
Miguelit09 <miguel.p0908@gmail.com>
2024-02-24 09:12:03 -0500

    Expresiones regulares

--- main.js
+++ main.js
@@ -4,97 +4,98 @@ const apellidos = document.getElementById("apellidos");
 const correo = document.getElementById("correo");
 const telefono = document.getElementById("telefono");
 
+let letra = /^[a-zA-Z]{3}_[0-9]{5}$/;
 
-function validarSoloNumeros(campo) {
-  if (campo.value.length < 4 || isNaN(campo.value)) {
-    if (campo.classList.contains("borde-verde")) {
-      campo.classList.remove("borde-verde");
-    }
-    campo.classList.add("borde-rojo");
-  } else {
-    if (campo.classList.contains("borde-rojo")) {
-      campo.classList.remove("borde-rojo");
-    }
-    campo.classList.add("borde-verde");
+const bordeVerde = function (campo) {
+  if (campo.classList.contains("borde-rojo")) {
+    campo.classList.remove("borde-rojo");
   }
+  campo.classList.add("borde-verde");
 }
 
-function validarSoloLetras(campo) {
-  if (campo.value.length < 4 || !/^[a-zA-Z]+$/.test(campo.value)) {
-    if (campo.classList.contains("borde-verde")) {
-      campo.classList.remove("borde-verde");
-    }
-    campo.classList.add("borde-rojo");
-  } else {
-    if (campo.classList.contains("borde-rojo")) {
-      campo.classList.remove("borde-rojo");
-    }
-    campo.classList.add("borde-verde");
+const bordeRojo = function (campo) {
+  if (campo.classList.contains("borde-verde")) {
+    campo.classList.remove("borde-verde");
   }
+  campo.classList.add("borde-rojo");
 }
 
-function validarCorreo(campo) {
-  if (!/^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/.test(campo.value)) {
-    if (campo.classList.contains("borde-verde")) {
-      campo.classList.remove("borde-verde");
-    }
-    campo.classList.add("borde-rojo");
-  } else {
-    if (campo.classList.contains("borde-rojo")) {
-      campo.classList.remove("borde-rojo");
-    }
-    campo.classList.add("borde-verde");
-  }
+const validarCampo = function (campo) {
+  (letra.test(campo.value)) ? bordeVerde(campo) : bordeRojo(campo)
+  // if (letra.test(campo.value)) {
+  //   if (campo.classList.contains("borde-rojo")) {
+  //     campo.classList.remove("borde-rojo");
+  //   }
+  //   campo.classList.add("borde-verde");
+  // } else {
+  //   if (campo.classList.contains("borde-verde")) {
+  //     campo.classList.remove("borde-verde");
+  //   }
+  //   campo.classList.add("borde-rojo");
+  // }
 }
-
 
 ficha.addEventListener('blur', () => {
-  validarSoloNumeros(ficha);
-})
-nombre.addEventListener('blur', () => {
-  validarSoloLetras(nombre);
-})
-apellidos.addEventListener('blur', () => {
-  validarSoloLetras(apellidos);
-})
-correo.addEventListener('blur', () => {
-  validarCorreo(correo);
-})
-telefono.addEventListener('blur', () => {
-  validarSoloNumeros(telefono);
+  validarCampo(ficha);
 })
 
-
-
-// for (let i = 0; i < campos.length; i++) {
-//   let validar = ""
-//   let campo = campos[i];
-//   if (i === 0 || i === 4) {
-//     validar = "numero";
-//   } else if (i === 3) {
-//     validar = "correo";
-//   } else {
-//     validar = "letra";
-//   }
-//   campo.addEventListener('blur', () => {
-//     if (validar === "numero") {
-//       validarSoloNumeros(campo);
-//     } else if (validar === "correo") {
-//       validarCorreo(campo);
-//     } else {
-//       validarSoloLetras(campo)
+// function validarSoloNumeros(campo) {
+//   if (campo.value.length < 4 || isNaN(campo.value)) {
+//     if (campo.classList.contains("borde-verde")) {
+//       campo.classList.remove("borde-verde");
 //     }
-//     // if (ficha.value.length < 4 || isNaN(ficha.value)) {
-//     //   if (ficha.classList.contains("borde-verde")) {
-//     //     ficha.classList.remove("borde-verde");
-//     //   }
-//     //   ficha.classList.add("borde-rojo");
-//     // } else {
-//     //   if (ficha.classList.contains("borde-rojo")) {
-//     //     ficha.classList.remove("borde-rojo");
-//     //   }
-//     //   ficha.classList.add("borde-verde");
-//     // }
-//   })
+//     campo.classList.add("borde-rojo");
+//   } else {
+//     if (campo.classList.contains("borde-rojo")) {
+//       campo.classList.remove("borde-rojo");
+//     }
+//     campo.classList.add("borde-verde");
+//   }
 // }
+
+// function validarSoloLetras(campo) {
+//   if (campo.value.length < 4 || !/^[a-zA-Z]+$/.test(campo.value)) {
+//     if (campo.classList.contains("borde-verde")) {
+//       campo.classList.remove("borde-verde");
+//     }
+//     campo.classList.add("borde-rojo");
+//   } else {
+//     if (campo.classList.contains("borde-rojo")) {
+//       campo.classList.remove("borde-rojo");
+//     }
+//     campo.classList.add("borde-verde");
+//   }
+// }
+
+// function validarCorreo(campo) {
+//   if (!/^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/.test(campo.value)) {
+//     if (campo.classList.contains("borde-verde")) {
+//       campo.classList.remove("borde-verde");
+//     }
+//     campo.classList.add("borde-rojo");
+//   } else {
+//     if (campo.classList.contains("borde-rojo")) {
+//       campo.classList.remove("borde-rojo");
+//     }
+//     campo.classList.add("borde-verde");
+//   }
+// }
+
+
+// ficha.addEventListener('blur', () => {
+//   validarSoloNumeros(ficha);
+// })
+// nombre.addEventListener('blur', () => {
+//   validarSoloLetras(nombre);
+// })
+// apellidos.addEventListener('blur', () => {
+//   validarSoloLetras(apellidos);
+// })
+// correo.addEventListener('blur', () => {
+//   validarCorreo(correo);
+// })
+// telefono.addEventListener('blur', () => {
+//   validarSoloNumeros(telefono);
+// })
+
 
